aa3607cd01c3664d4dc626e6593e74037d49d93f
Masato Ikeda <masato.ikeda@gmail.com>
2019-05-19 00:15:29 +0900

    Implement better_linear_search in C

---
+++ 2/better_linear_search.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define SIZE 100
+#define NOT_FOUND -1
+
+int linear_search(int *a, int size, int target) {
+  for (int i = 0; i < size; i++) {
+    if (a[i] == target) {
+      return i;
+    }
+  }
+  return NOT_FOUND;
+}
+
+int main(int argc, char **args) {
+  int a[SIZE], result;
+  for (int i = 0; i < SIZE; i++) {
+    a[i] = i;
+  }
+
+  result = linear_search(a, SIZE, 8);
+  if (result != 8) {
+    printf("Expected %d but got %d", 8, result);
+    return 1;
+  }
+
+  result = linear_search(a, SIZE, 100);
+  if (result != NOT_FOUND) {
+    printf("Expected %d but got %d", NOT_FOUND, result);
+    return 1;
+  }
+
+  return 0;
+}
